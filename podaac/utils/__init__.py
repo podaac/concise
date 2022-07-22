6e987d2c2eb2d5f6ba5eed64f2d3307f62f47bac
Zoltan Miskolci <zoltan.miskolci@jpl.nasa.gov>
2022-07-21 17:00:29 -0700

    updated flake8 to ignore init.py

--- podaac/utils/__init__.py
+++ podaac/utils/__init__.py
@@ -1,4 +1,3 @@
 # pylint: disable=C0114
-# pylint: disable=F0401
 
 from podaac.utils.cumulus_api import CumulusAPI
--- podaac/utils/enums/__init__.py
+++ podaac/utils/enums/__init__.py
@@ -1,4 +1,3 @@
 # pylint: disable=C0114
-# pylint: disable=F0401
 
 from podaac.utils.enums.venue import Venue
